23e834d9eb004279dae3d895112d69a24aee6202
paigelynnv <64274185+paigelynnv@users.noreply.github.com>
2021-10-04 15:47:29 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Weather from "./weather-app";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Weather />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
